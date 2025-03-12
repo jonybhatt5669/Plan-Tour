922d1d59c737578c95d1082d9739dab6f1c50f7f
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-03-13 00:19:56 +0600

    Fix prettier issue with eslint

--- prettier.config.js
+++ prettier.config.js
@@ -4,6 +4,7 @@ module.exports = {
   singleQuote: true,
   bracketSameLine: true,
   trailingComma: 'es5',
+  endOfLine: true,
 
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
   tailwindAttributes: ['className'],
