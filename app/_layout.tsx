4132a214101768db150f6deb5048c0910bddc762
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-03-12 18:45:31 +0600

    Added font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,63 @@
 import '../global.css';
 
+import {
+  Lora_400Regular as Lora_Regular,
+  Lora_500Medium as Lora_Medium,
+  Lora_600SemiBold as Lora_SemiBold,
+  Lora_700Bold as Lora_Bold,
+} from '@expo-google-fonts/lora';
+import {
+  useFonts,
+  Philosopher_400Regular as Phil_Regular,
+  Philosopher_700Bold as Phil_Bold,
+} from '@expo-google-fonts/philosopher';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
+SplashScreen.preventAutoHideAsync();
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    Phil_Bold,
+    Phil_Regular,
+    Lora_Regular,
+    Lora_Medium,
+    Lora_SemiBold,
+    Lora_Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
+
+/**
+ * Philosopher_400Regular
+Philosopher_400Regular_Italic
+Philosopher_700Bold
+Philosopher_700Bold_Italic
+
+import {  } from '@expo-google-fonts/philosopher'
+
+Lora_400Regular
+Lora_500Medium
+Lora_600SemiBold
+Lora_700Bold
+Lora_400Regular_Italic
+Lora_500Medium_Italic
+Lora_600SemiBold_Italic
+Lora_700Bold_Italic
+
+import {  } from '@expo-google-fonts/lora'
+ */
