87f09501c4c7089c960ab32753061f6151504b43
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-03-17 22:02:25 +0600

    Added screen

--- app/onboarding/_layout.tsx
+++ app/onboarding/_layout.tsx
@@ -1,14 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { Stack } from 'expo-router';
+
+import { OnboardingProvider } from '~/context/onboardingContext';
 
 export default function OnboardingLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <OnboardingProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="phone-input"
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen name="verification-code" options={{ headerShown: false }} />
+      </Stack>
+    </OnboardingProvider>
   );
 }
