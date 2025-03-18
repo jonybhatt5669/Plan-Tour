5820e78b396c528f1e62c9a23b22aa0976e08272
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-03-18 21:32:10 +0600

    Added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Phil-Bold': ['Phil_Bold', 'san-serif'],
+      },
+    },
   },
   plugins: [],
 };
