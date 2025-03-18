6d4bf9555f805135f3c7d71423ef0874c7ee5e77
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-03-19 02:48:10 +0600

    Update design

--- app/onboarding/verification-code.tsx
+++ app/onboarding/verification-code.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import { useRef, useState } from 'react';
-import { View, Text, Pressable, TextInput } from 'react-native';
+import { View, Text, Pressable, TextInput, Image } from 'react-native';
 import { useOnboarding } from '~/context/onboardingContext';
 export default function VerificationCode() {
   const { setStep } = useOnboarding();
@@ -41,16 +42,34 @@ export default function VerificationCode() {
     setStep(1);
     router.push('/onboarding/phone-input');
   };
+  const logo = require('../../assets/images/logo.png');
 
   return (
     <View className="flex-1">
-      <Pressable onPress={handleBack} className="flex-row items-center gap-2 px-4 py-4">
-        <AntDesign name="arrowleft" size={20} color="black" />
-        <Text className="text-lg font-semibold text-[Phil_Bold]">Verification Code</Text>
-      </Pressable>
+      <LinearGradient
+        colors={['#5d5d3c', '#557c93']}
+        className="w-full flex-row items-center justify-center"
+        style={{
+          borderBottomLeftRadius: 60,
+          borderBottomRightRadius: 60,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 0,
+            height: 10,
+          },
+          shadowOpacity: 0.51,
+          shadowRadius: 13.16,
+
+          elevation: 20,
+        }}>
+        <Image source={logo} alt="Logo" className="h-24 w-24" resizeMode="cover" />
+        <Text className="text-3xl font-bold text-white">Plan Tour</Text>
+      </LinearGradient>
       <View className="mt-8 flex-1 px-4">
-        <Text className="text-2xl font-semibold">Enter the code</Text>
-        <View className="mt-4 w-full flex-row items-center gap-4">
+        <Text className="mb-4 text-center font-Phil-Bold text-5xl font-semibold">
+          Enter the code
+        </Text>
+        <View className="mt-4 w-full flex-row items-center justify-center gap-4">
           {otp.map((digit, index) => (
             <TextInput
               key={index}
@@ -68,11 +87,16 @@ export default function VerificationCode() {
           ))}
         </View>
       </View>
-      <Pressable onPress={handleNext} className="items-end justify-end  px-8 py-8">
-        <View className="size-16 items-center justify-center rounded-full bg-black">
-          <AntDesign name="arrowright" size={24} color="white" />
-        </View>
-      </Pressable>
+      <View className="flex-row items-center justify-between px-8 py-8">
+        <Pressable onPress={handleBack} className="">
+          <AntDesign name="arrowleft" size={24} color="black" />
+        </Pressable>
+        <Pressable onPress={handleNext} className=" ">
+          <View className="size-16 items-center justify-center rounded-full bg-black">
+            <AntDesign name="arrowright" size={24} color="white" />
+          </View>
+        </Pressable>
+      </View>
     </View>
   );
 }
