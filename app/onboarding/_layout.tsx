d1cbaec48b5994157f71a707fcfea89db53d52ca
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-03-18 19:04:50 +0600

    Added location tracker

--- app/onboarding/_layout.tsx
+++ app/onboarding/_layout.tsx
@@ -21,6 +21,7 @@ export default function OnboardingLayout() {
         />
 
         <Stack.Screen name="verification-code" options={{ headerShown: false }} />
+        <Stack.Screen name="location-tracker" options={{ headerShown: false }} />
       </Stack>
     </OnboardingProvider>
   );
